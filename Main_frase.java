7e7bd842cc0b97afd33ff9922f3e9fd924811f85
thebasto17 <info.marc32@gmail.com>
2016-11-09 12:16:18 +0100

    Add files via upload

--- Main_frase.java
+++ Main_frase.java
@@ -24,46 +24,61 @@ public class Main_frase {
      */
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
+   
+        int num;
         System.out.println("Classe Frase");
-        System.out.println("Introdueix una frase");
-        Scanner lector = new Scanner(System.in); 
-        Frase f = new Frase();
-        String option = lector.nextLine();
+        while(true){                 
         
-        System.out.println("Utilitzem el set per afegir a la frase l'string");
-        f.set_frasestring(option);
-        System.out.println("Mostrem la frase amb la funció imprimir");
-        f.imprimir();
-        System.out.println("Dividim la frase en paraules i la mostrem");
-        f.dividir();
-        f.imprimir_llista_paraules();
-        
-        List<Paraula> ls;
-        ls = new ArrayList();
-        ls = f.get_lp();
-        //f.set_lpar(ls);
-        System.out.println("Provem el get i el set de la llista i el mostrem");
-        for(Paraula pa : ls) {
-            pa.imprimir();
+            System.out.println("Introdueix una nova frase");
+            Scanner lector = new Scanner(System.in); 
+            Frase f = new Frase();
+            String option = lector.nextLine();
+            f.set_frasestring(option);
+            
+            System.out.println("Escriu un numero del 1 al 5.");
+            System.out.println("1. Afegeix una paraula a la frase");
+            System.out.println("2. Funcio set i imprimir");
+            System.out.println("3. Dividir la frase en paraules i imprimir llista");
+            System.out.println("4. Retornem el nombre de paraules de la frase");
+            System.out.println("5. Funcio get i imprimir en llista");
+            
+            String opt = lector.nextLine();
+            
+            num = Integer.parseInt(opt);
+            
+            switch(num)
+            {
+                case 1: Paraula p = new Paraula();
+                        System.out.println("Introdueix una paraula");
+                        String opt2 = lector.nextLine();
+                        p.set_p(opt2);
+                        f.afegir(p);
+                        f.imprimir(); break;
+                        
+                case 2: f.set_frasestring(option);
+                        f.imprimir(); break;
+                        
+                case 3: f.dividir();
+                        f.imprimir_llista_paraules(); break;
+                        
+                case 4: f.dividir();
+                        int len = f.nombre_paraules();
+                        System.out.println(len); break;
+                        
+                case 5: List<Paraula> ls;
+                        ls = new ArrayList();
+                        f.dividir();
+                        ls = f.get_lp();
+                        for(Paraula pa : ls) {
+                            pa.imprimir();
+                        }break;                   
+                
+                default: System.out.println("Escriu un numero del 1 al 5."); break;
+
+            }
         }
         
-        System.out.println("Convertim les paraules de la frase en string");
-        Frase fr = new Frase();
         
-        Paraula p = new Paraula();
-        System.out.println("Introdueix una paraula");
-        String opt = lector.nextLine();
-        p.set_p(opt);
-        //System.out.println("Creem una frase a partir d'una paraula");
-        //Frase fr = new Frase();
-        System.out.println("Afegim una paraula a la frase: ");
-        f.afegir(p);
-        f.imprimir();
-        
-        System.out.println("Retornem el nombre de paraules de la frase");
-        int len = f.nombre_paraules();
-        System.out.println(len);
-    
         
         
         
