a6949d0fbee2df5d6a3a22073892e09d11d681f4
Duje Čop <sergiokovf1champs@gmail.com>
2016-11-11 00:36:06 +0100

    Sergio 11/11/16 00:35
    FALTA ACABAR LA BÚSQUEDA DE DOCUMENTS SEMBLANTS!

--- Cjt_Documents.java
+++ Cjt_Documents.java
@@ -5,7 +5,10 @@
  */
 package gestordocuments;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
 import javafx.util.Pair;
 /**
  *
@@ -110,18 +113,20 @@ public class Cjt_Documents {
         }
         else System.out.println("ERROR: No hi ha cap Document.");
     }
-
+    
     public void esborrarDoc(String tit, String aut) {
         if(!cjt.isEmpty()) {
             boolean trobat = false;
             int i = 0;
-            while(!trobat && i < cjt.size()) {
-                if(cjt.get(i).get_titol().get_frasestring().equalsIgnoreCase(tit)) {
-                    if(cjt.get(i).get_autor().get_frasestring().equalsIgnoreCase(aut)) {
-                        cjt.remove(i);
-                        if(i == max_id) recalcular_maxid();
-                        trobat = true;
-                        System.out.println("Operació realitzada amb èxit.");
+            while(!trobat && i <= max_id) {
+                if(cjt.get(i) != null) {
+                    if(cjt.get(i).get_titol().get_frasestring().equalsIgnoreCase(tit)) {
+                        if(cjt.get(i).get_autor().get_frasestring().equalsIgnoreCase(aut)) {
+                            cjt.remove(i);
+                            if(i == max_id) recalcular_maxid();
+                            trobat = true;
+                            System.out.println("Operació realitzada amb èxit.");
+                        }
                     }
                 }
                 ++i;
@@ -131,7 +136,148 @@ public class Cjt_Documents {
         else System.out.println("ERROR: El conjunt de documents és buit.");
     }
     
-    //public void cercar();
+    public void modificar(int id) {
+        if(cjt.get(id) != null) {
+            System.out.println("Vols modificar: Atributs o Contingut?");
+            Scanner lector = new Scanner(System.in);
+            String option = lector.nextLine();
+            if(option.equalsIgnoreCase("atributs")) {
+                System.out.println("Introdueix un nou títol, el seu autor i el seu tema.");
+                String ti = lector.nextLine();
+                String au = lector.nextLine();
+                String te = lector.nextLine();
+
+                cjt.get(id).set_titol_String(ti);                
+                cjt.get(id).set_autor_String(au);                
+                cjt.get(id).set_tema_String(te);
+                
+                System.out.println("Operació realitzada amb èxit.");
+            }
+            else if(option.equalsIgnoreCase("contingut")) {
+                System.out.println("Introdueix el nou contingut del document.");
+                String co = lector.nextLine();
+                
+                cjt.get(id).set_contingut_String(co);
+                System.out.println("Operació realitzada amb èxit.");
+            }
+            else System.out.println("ERROR: Comanda incorrecta.");
+        }
+        else System.out.println("ERROR: El document no existeix.");
+    }
+    
+    public void modificarDoc(String tit, String aut) {
+        boolean trobat = false;
+        int i = 0;
+        int id = 0;
+        while(!trobat && i <= max_id) {
+            if(cjt.get(i) != null) {
+                if(cjt.get(i).get_titol().get_frasestring().equalsIgnoreCase(tit)) {
+                    if(cjt.get(i).get_autor().get_frasestring().equalsIgnoreCase(aut)) {
+                        trobat = true;
+                        id = i;
+                    }
+                }
+            }
+            ++i;
+        }
+                        
+        if(trobat) {                
+            System.out.println("Vols modificar: Atributs o Contingut?");
+            Scanner lector = new Scanner(System.in);
+            String option = lector.nextLine();
+            if(option.equalsIgnoreCase("atributs")) {
+                System.out.println("Introdueix un nou títol, el seu autor i el seu tema.");
+                String ti = lector.nextLine();
+                String au = lector.nextLine();
+                String te = lector.nextLine();
+
+                cjt.get(id).set_titol_String(ti);                
+                cjt.get(id).set_autor_String(au);                
+                cjt.get(id).set_tema_String(te);
+                
+                System.out.println("Operació realitzada amb èxit.");
+            }
+            
+            else if(option.equalsIgnoreCase("contingut")) {
+                System.out.println("Introdueix el nou contingut del document.");
+                String co = lector.nextLine();
+                
+                cjt.get(id).set_contingut_String(co);
+                System.out.println("Operació realitzada amb èxit.");
+            }
+            else System.out.println("ERROR: Comanda incorrecta.");
+        }
+        else System.out.println("ERROR: El document no existeix.");
+    }
+    
+    public void cerca_Autor(String autor) {
+        System.out.println("Llista de documents escrits per " + autor + ':');
+        for(int i = 0; i <= max_id; ++i) {
+            if(cjt.get(i) != null) {
+                if(cjt.get(i).get_autor().get_frasestring().equalsIgnoreCase(autor)) {
+                    System.out.println(cjt.get(i).get_titol().get_frasestring());
+                }
+            }
+        }
+    }
+    
+    public void cerca_Prefix(String prefix) {
+        Set<String> autors = new HashSet();
+        System.out.println("Llista de autors que comencen per " + prefix + ':');
+        for(int i = 0; i <= max_id; ++i) {
+            if(cjt.get(i) != null) {
+                if(cjt.get(i).get_autor().get_frasestring().toLowerCase().startsWith(prefix.toLowerCase())) {
+                    if(autors.isEmpty()) autors.add(cjt.get(i).get_autor().get_frasestring());
+                    else {
+                        if(!autors.contains(cjt.get(i).get_autor().get_frasestring())) autors.add(cjt.get(i).get_autor().get_frasestring());
+                    }
+                }
+            }
+        }
+        for(String ss : autors) {
+            System.out.println(ss);
+        }
+    }
+    
+    public void cerca_Document(String titol, String autor) {
+        System.out.println("El contingut del document buscat és:");
+        for(int i = 0; i <= max_id; ++i) {
+            if(cjt.get(i) != null) {
+                if(cjt.get(i).get_titol().get_frasestring().equalsIgnoreCase(titol)){
+                    if(cjt.get(i).get_autor().get_frasestring().equalsIgnoreCase(autor)) {
+                        System.out.println(cjt.get(i).get_contingut().get_textstring());
+                    }
+                }
+            }
+        }
+    }
+    
+    public void cerca_Semblants(Document d, int k) {
+        String contenido = d.get_contingut().get_textstring();
+        
+        //SIUSPLAU COMPLETEU AIXÒ, SI SABEU COM EXPLICAR BREUMENT CADA MÈTODE.
+        System.out.println("De l'1 al 3, quin mètode vols?");
+        System.out.println("1. ***COMPLETAR***");
+        System.out.println("2. ***COMPLETAR***");
+        System.out.println("3. ***COMPLETAR***");
+        
+        Scanner lector = new Scanner(System.in);
+        int num = lector.nextInt();
+        switch (num) { //FALTA COMPLETAR CADA CAS... SORRY
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default: System.out.println("ERROR: Ha de ser un nº entre 1 i 3!");
+                break;
+        }
+        
+        //DESPRÉS DE LO DE DALT CALDRÀ RETORNAR LA LLISTA DE K DOCUMENTS SEMBLANTS A D.
+        
+        System.out.println("Operació realitzada amb èxit.");
+    }
     
     public boolean existeix(Document d) {
         boolean existeix = false;
@@ -153,5 +299,9 @@ public class Cjt_Documents {
         boolean trobat = false;
         if(cjt.get(id) != null) trobat = true;
         return trobat;
+    }
+    
+    public int assignar_id_fitxer() { //NO SÉ SI AIXÒ ESTÀ BÉ (= CRITERI QUE PER ID DOCUMENT)
+        return trobar_id_disponible();
     }
 }
--- main.java
+++ main.java
@@ -6,10 +6,7 @@
 package gestordocuments;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.List;
-import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -24,8 +21,8 @@ public class main {
      */
     public static void main(String[] args) throws FileNotFoundException, IOException {
         
-        System.out.println("Benvingut al Gestor de Documents.");
-        System.out.println("Escull una ordre. (Afegir/Esborrar/Imprimir/Consultar/Sortir)");
+        System.out.println("Benvingut al Gestor de Documents. ('Sortir' per acabar)");
+        System.out.println("Escull una ordre. (Afegir/Esborrar/Modificar/Imprimir/Consultar)");
         Cjt_Documents cjt = new Cjt_Documents();
         Scanner lector = new Scanner(System.in);
         String option = lector.nextLine();
@@ -39,20 +36,42 @@ public class main {
                 cjt.afegir(d);
             }
             else if(option.equalsIgnoreCase("Esborrar")) {
-                System.out.println("Escull opció d'esborrat (ID/Nom).");
-                String opt = lector.nextLine();
-                if(opt.equalsIgnoreCase("ID")) {
-                    System.out.println("Introdueix l'ID del document a esborrar.");
-                    int num = lector.nextInt();
-                    cjt.esborrar(num);
+                if(cjt.get_cjt().isEmpty()) System.out.println("ERROR: El conjunt de documents és buit.");
+                else {
+                    System.out.println("Escull opció d'esborrat (ID/Nom).");
+                    String opt = lector.nextLine();
+                    if(opt.equalsIgnoreCase("ID")) {
+                        System.out.println("Introdueix l'ID del document a esborrar.");
+                        int num = lector.nextInt();
+                        cjt.esborrar(num);
+                    }
+                    else if(opt.equalsIgnoreCase("Nom")) {
+                        System.out.println("Introdueix el títol i l'autor del document a esborrar.");
+                        String tit = lector.nextLine();
+                        String aut = lector.nextLine();
+                        cjt.esborrarDoc(tit, aut);
+                    }
+                    else System.out.println("ERROR: Comanda incorrecta.");
                 }
-                else if(opt.equalsIgnoreCase("Nom")) {
-                    System.out.println("Introdueix el títol i l'autor del document a esborrar.");
-                    String tit = lector.nextLine();
-                    String aut = lector.nextLine();
-                    cjt.esborrarDoc(tit, aut);
+            }
+            else if(option.equalsIgnoreCase("Modificar")) {
+                if(cjt.get_cjt().isEmpty()) System.out.println("ERROR: El conjunt de documents és buit.");
+                else {
+                    System.out.println("Escull opció de modificació (ID/Nom).");
+                    String opt = lector.nextLine();
+                    if(opt.equalsIgnoreCase("ID")) {
+                        System.out.println("Introdueix l'ID del document a modificar.");
+                        int num = lector.nextInt();
+                        cjt.modificar(num);
+                    }
+                    else if(opt.equalsIgnoreCase("Nom")) {
+                        System.out.println("Introdueix el títol i l'autor del document a modificar.");
+                        String tit = lector.nextLine();
+                        String aut = lector.nextLine();
+                        cjt.modificarDoc(tit, aut);
+                    }
+                    else System.out.println("ERROR: Comanda incorrecta.");
                 }
-                else System.out.println("ERROR: Comanda incorrecta.");
             }
             else if(option.equalsIgnoreCase("Imprimir")) {
                 System.out.println("Introdueix l'ID del document a imprimir.");
@@ -64,7 +83,78 @@ public class main {
                 else System.out.println("ERROR: El document no existeix.");
             }
             else if(option.equalsIgnoreCase("Consultar")) {
-                
+                if(cjt.get_cjt().isEmpty()) System.out.println("ERROR: El conjunt de documents és buit.");
+                else {    
+                    System.out.println("Selecciona el tipus de consulta (per nº):");
+                    System.out.println("1. Buscar títols d'un autor");
+                    System.out.println("2. Buscar autors per prefix");
+                    System.out.println("3. Buscar contingut per títol i autor");
+                    System.out.println("4. Buscar K documents més semblants a D");
+
+                    String num = lector.nextLine();
+                    int opt = Integer.parseInt(num);
+
+                    switch(opt) {
+                        case 1:
+                            System.out.println("Introdueix el nom de l'autor:");
+                            String nombre = lector.nextLine();
+                            cjt.cerca_Autor(nombre);
+                            System.out.println("Operació realitzada amb èxit.");
+                            break;
+                        case 2:
+                            System.out.println("Introdueix el prefix per buscar:");
+                            String prefix = lector.nextLine();
+                            cjt.cerca_Prefix(prefix);
+                            System.out.println("Operació realitzada amb èxit.");
+                            break;                        
+                        case 3:
+                            System.out.println("Introdueix el titol i l'autor del document a buscar:");
+                            String tt = lector.nextLine();
+                            String at = lector.nextLine();
+                            cjt.cerca_Document(tt, at);
+                            System.out.println("Operació realitzada amb èxit.");                            
+                            break;
+                        case 4:
+                            System.out.println("Escull entre atributs o ID:");
+                            String opc = lector.nextLine();
+                            if(opc.equalsIgnoreCase("id")) {
+                                System.out.println("Introdueix l'ID del document:");
+                                int ide = lector.nextInt();
+                                System.out.println("Quants documents semblants vols?");
+                                int k = lector.nextInt();
+                                if(cjt.get_cjt().get(ide) == null) {
+                                    System.out.println("ERROR: El document no existeix.");
+                                    break;
+                                }
+                                Document d = cjt.get_cjt().get(ide);
+                                cjt.cerca_Semblants(d, k);
+                                System.out.println("Operació realitzada correctament.");
+                            }
+                            else if(opc.equalsIgnoreCase("atributs")) {
+                                System.out.println("Introdueix el títol i l'autor del document:");
+                                String titulo = lector.nextLine();
+                                String autoria = lector.nextLine();
+                                Document doc = new Document();
+                                for(Document d : cjt.get_cjt().values()) {
+                                    if(d.get_titol().get_frasestring().equalsIgnoreCase(titulo)) {
+                                        if(d.get_autor().get_frasestring().equalsIgnoreCase(autoria)) {
+                                            doc = d;
+                                        }
+                                    }
+                                }
+                                
+                                System.out.println("Quants documents semblants vols?");
+                                int k = lector.nextInt();           
+                                
+                                cjt.cerca_Semblants(doc, k);
+                                System.out.println("Operació realitzada correctament.");
+                            }
+                            else System.out.println("ERROR: Comanda incorrecta.");
+                            break;
+                        default: System.out.println("ERROR: Ha de ser un nº entre 1 i 4!");
+                            break;
+                    }
+                }
             }
             option = lector.nextLine();
         }
