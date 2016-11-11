f2088eee837b108f4cb668917699aef062265894
Erham <adria_fors@hotmail.com>
2016-11-11 07:26:42 +0100

    Update Espai_vectorial.java

--- Espai_vectorial.java
+++ Espai_vectorial.java
@@ -3,7 +3,6 @@ package fitxers;
 import static fitxers.Fitxer.path;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -20,38 +19,39 @@ public class Espai_vectorial extends Fitxer {
     boolean modificacio;
     */
     
-    private /* final */ HashMap<String,Integer> paraules;           // map amb paraula - numero de cops que apareix
-    private /* final */ ArrayList<Map.Entry<String,Integer>> llista; // llista amb numero aparicions - paraula
+    private HashMap<String,Integer> paraules;            // map amb paraula - numero de cops que apareix
+    private ArrayList<Map.Entry<String,Integer>> llista; // llista amb numero aparicions - paraula
     private int dimensio;
     
+    //  Instancia un Espai_vectorial
+    //  Si "espai_vectorial.txt" no estava creat, el crea. Si ja estava creat:
+    //  llegeix el contingut de "espai_vectorial.txt" i l'assigna a "text"
     public Espai_vectorial() throws IOException {
         
-        this.nom = new String();
-        this.text = new String();
+        this.ini();
+        File a = new File(path(),this.nom + ".txt");
         
-        this.nom = "espai_vectorial";
-        this.presencia = false;
-        this.modificacio = false;
-        
-        File a = new File(path(),this.nom + ".txt");       
-        BufferedWriter bufw;
-        
-        if (!a.exists()) {
-            bufw = new BufferedWriter(new FileWriter(a)); // registre creat
-            bufw.close();
+        if (!a.exists()) {  // si "registre.txt" no estava creat:
+            this.crear(a);
         }
-        
-        if (a.exists()) { this.presencia = true; }
-        ///// AQUESTA FUNCIÓ POT SER ELIMINABLE PER INCLOURE ALGO EN FITXER QUE HO FACI ????? DEL PAL PER NO REPETIR CODI!!!!!!!!!!!!
-        ///// AQUEST FUNCIÓ ÉS IGUAL QUE LA DE REGISTRE!!!!!!!
-        
-        
+        else {
+            presencia = true;
+            this.llegir();
+            this.omplir_map();
+            this.omplir_list();
+        }
+         
+    }
+    
+    //  Assigna el "nom" que sempre es "espai_vectorial" i assigna "dimensio" i "modificacio"
+    private void ini() {
+        nom = "espai_vectorial";
         paraules = new HashMap<>();
         llista = new ArrayList<>();
-        
-        this.dimensio = 4; /* ESCOLLIR LA DIMENSIÓ */
-        
+        dimensio = 10;
+        modificacio = false;
     }
+    /*    === === ===  ESCOLLIR LA DIMENSIÓ  === === ===    */
     
     public int get_dimensio() {
         return this.dimensio;
@@ -61,17 +61,9 @@ public class Espai_vectorial extends Fitxer {
         this.dimensio = dim;
     }
     
-    public void llegir() {
-        
-    }
-    
-    public void guardar() {
-        
-    }
-    
     //  Omple el map "paraules" agafant tots els paraula_frequencia del String "text"
     //  Si "text" és buit ho comunica
-    public void omplir_map() {
+    private void omplir_map() {
         int size = (this.text).length();  
         if (size > 0) {        // executar només si el String és > 0, si no, vol dir que a l'espai no hi havia paraules (encara)
             (this.paraules).clear();
@@ -99,6 +91,11 @@ public class Espai_vectorial extends Fitxer {
         else {
             System.out.println("- A L'ESPAI NO HI HA PARAULES !!! -");
         }
+    }
+    
+    //  Aquesta funcio nomes es pels drivers
+    public void esborrar_map() {
+        (this.paraules).clear();
     }
     
     //  Imprimeix el contingut del map "paraules" [clau=valor,...]
@@ -152,13 +149,12 @@ public class Espai_vectorial extends Fitxer {
                 // extreure
                 
                     Integer freq = (this.paraules).get(paraula);    // obtenim la frequencia, sempre ha de donar 1 almenys
-                    if (freq != null) { // sempre
+                    if (freq != null) { // sempre, si la paraula hi es
                             (this.paraules).replace(paraula,--freq);    // -- a la frequencia d'aparicio de la paraula
                             (this.paraules).remove(paraula,0);          // L'esborrem si la frequencia era 0
                     }
                     else {
-                        System.out.println("ERROR 37");
-                        // TANCAR AQUESTA "SESSIÓ" DEL MAP, ÉS A DIR, ELIMINAR L'OBJECTE, NO GUARDAR-LO AL DISC!!!
+                        System.out.println("ERROR");
                     }
                 // end extreure
                 }
@@ -171,7 +167,7 @@ public class Espai_vectorial extends Fitxer {
     
     //  Buida "llista". Omple "llista" amb els valors de "paraules"
     //  Ordena "llista" de més gran a més petit segons la frequencia de cada paraula
-    public void omplir_list() {
+    private void omplir_list() {
         (this.llista).clear();
         Iterator it = (this.paraules).entrySet().iterator();
         Map.Entry<String,Integer> element;
@@ -246,29 +242,40 @@ public class Espai_vectorial extends Fitxer {
         return this.calcular_vector(doc, false);
     }
     
+    //  Substitueix el contingut de espai_vectorial.txt per un altre amb el seguent format:
+    //  paraula1 frequencia_d'aparicio1 paraula2 frequencia_d'aparicio2 ... paraula-n frequencia_d'aparicio-n
+    //  On les paraules 1..n son totes les presents a "paraules" amb la seva corresponent frequencia d'aparicio (el value)
+    //  Basicament es una funcio per guardar el contingut de l'objecte en espai_vectorial.txt
+    //  (espai_vectorial.txt ha d'estar creat)
+    @Override
+    public void guardar() throws IOException {
+        if (this.modificacio) {
+            this.buidar_text();
+            boolean primer = true;
+            
+            Iterator it = (paraules).entrySet().iterator();
+            Map.Entry<String,Integer> element;
+            
+            while (it.hasNext()) {
+                element = (Map.Entry<String,Integer>) it.next();   // obtenim una entrada del map
+                
+                if (primer) {
+                    text = text.concat(element.getKey())  + " ";
+                    primer = false;
+                }
+                else {
+                    text = text.concat(" " + element.getKey() + " ");     // afegim al "text" la key (paraula) + un espai per posar despres l'enter
+                }
+                text = text.concat(Integer.toString(element.getValue()));
+            }
+            // Ara tenim "text" ple
+            BufferedWriter bw = obrir_fitxer_w();
+            if (bw != null) {
+                bw.write(this.text);
+                bw.close();
+                this.modificacio = false;
+            }
+        }   
+    }
     
 }
-
-/*
-    boolean contains(Object o)
-    int indexOf(Object o)
-    E get(int index)
-
-getValue()
-
-        */
-
-/*
-
-- llegir espai vectorial
-- guardar espai vectorial
-- * actualitzar afegint document espai vectorial, segons un string (l'string a rebre ha de ser una tira de paraules PERFECTA, paraula1_paraula2_..._paraula-n i NET)
-- * actualitzar traient de l'espai vectorial (ídem)
-- * donat un text string NET, calcular el seu vector (sistema 1, sistema 2 i sistema 3, segons el nombre de paraules a considerar [canviable? segons la dimensió] )
-     el vector s'emet com una llista d'enters
-
-
-FORMAT en .txt , que correspon al text:
-paraula numero paraula numero paraula numero
-
-*/
