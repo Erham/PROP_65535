c09777bec5e284dba0c249ebbf0f637741d61d56
Erham <adria_fors@hotmail.com>
2016-11-11 07:23:01 +0100

    Update Arxiu.java

--- Arxiu.java
+++ Arxiu.java
@@ -1,17 +1,11 @@
 package fitxers;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Arxiu extends Fitxer {
     
-    //private String nom;     // nom de fitxer .txt (id)
+    //private String nom
     
     private String titol;
     private String autor;
@@ -19,38 +13,68 @@ public class Arxiu extends Fitxer {
     
     // private String text;
     
-    // private boolean presencia;      // fals en crear la instància
-    // private boolean modificacio;    // fals en inici, cert si canvies algo de l'arxiu
+    // private boolean presencia;
+    // private boolean modificacio;
     
-    //====================================//
     
-    public Arxiu() {        // Creadora per defecte
+    //  Instancia un Arxiu
+    //  Es la creacio per defecte, no s'hauria de fer servir, pero es util per fer proves
+    public Arxiu() {
         
-        this.nom = new String();    // en crear el fitxer creem els new's encara que potser no cal
+        this.ini();     // new - titol, autor, tema
+        tema = "t_null";   // tema per defecte
         
-        // this.nom = "42";
-        
-        this.titol = new String();
-        
-        // this.titol = "titol";
-                
-        this.autor = new String();
-        
-        // this.autor = "autor";
-        
-        this.tema = new String();
-        this.tema = "t_null";   // el tema per defecte
-        
-        this.text = new String();
-        
-        this.presencia = false;
-        this.modificacio = true;
+        presencia = false;
+        modificacio = true;
+        // this.set_text("abcd");
     }
     
+    //  Instancia un Arxiu amb identificador "id"
+    //  Carrega el contingut del fitxer "id".txt a l'Arxiu instanciat
+    //  Serveix per obtenir Arxius amb el "text" corresponent al seu fitxer .txt ja assignat per poder-lo consultar
+    //  (El fitxer "id".txt JA HA D'ESTAR CREAT ABANS, si no l'Arxiu creat no és vàlid, i s'imprimeix un missatge)
+    public Arxiu(int id) throws IOException {
+        
+        this.ini();     // new - titol, autor, tema
+        nom = Integer.toString(id);
+        File a = new File(path(),nom + ".txt");
+        if (a.exists()) {
+            presencia = true;
+            modificacio = false;
+            llegir();   // llegeix el .txt
+        }
+        else {
+            System.out.println("No existeix el fitxer .txt amb aquest identificador");
+        }
+        
+    }
     
-    @Override
-    public Object clone() throws CloneNotSupportedException {
-        return super.clone();
+    //  Intancia un Arxiu amb identificador "id", "titol" = "titol_1", "autor" = "autor_1" i "tema" = "tema_1"
+    //  Crea un fitxer "id".txt a la carpeta on siguis (a la carpeta path())
+    //  Serveix per obtenir Arxius amb tots els atributs assignats excepte el "text", i que ja tinguin el fitxer .txt creat
+    //  (El fitxer "id".txt NO HA D'ESTAR CREAT ABANS, si ho estava, l'arxiu creat no és vàlid, i s'imprimeix un missatge)
+    public Arxiu(int id, String titol_1, String autor_1, String tema_1) throws IOException {
+        
+        this.ini();     // new - titol, autor, tema
+        nom = Integer.toString(id);
+        File a = new File(path(),nom + ".txt");
+        if (a.exists()) {
+            System.out.println("Ja existeix un fitxer .txt amb aquest identificador");
+        }
+        else { // no existeix
+            crear(a);
+            titol = titol_1;
+            autor = autor_1;
+            tema = tema_1;
+        }
+        
+    }
+    
+    //  Fa els new de titol, autor i tema
+    private void ini() {
+        titol = new String();
+        autor = new String();
+        tema = new String();
     }
     
     //====================================//
@@ -99,71 +123,4 @@ public class Arxiu extends Fitxer {
         this.nom = aux;
     }
     
-    
-//==================================================//
-//==================================================//
-    
-    
-    // PRE: L'arxiu té el nom (l'id) ja assignat i el tema (o t_null per defecte)
-    //      No existeix cap arxiu amb aquest id a la col·lecció de documents
-    public void crear() throws IOException {
-        File a = new File(path(),this.nom + ".txt");       
-        BufferedWriter bufw;
-        
-        if (a.exists()) {
-            System.out.println("l'arxiu JA existeix");
-        } else {
-            bufw = new BufferedWriter(new FileWriter(a)); // .txt creat
-            bufw.close();
-            this.presencia = true;
-            this.modificacio = true;
-        }
-    }
-    // POST: el .txt ha estat creat a la carpeta del projecte (BUIT)
-    
-    // PRE: El .txt en concret ja està creat i l'arxiu té el nom
-    public void guardar_text() throws IOException {
-        if (this.modificacio) {
-            BufferedWriter bw = obrir_fitxer_w();
-            if (bw != null) {
-                bw.write(this.text);
-                bw.close();
-            this.modificacio = false;
-            }
-        }
-    }
-    // POST: Esborra el contingut del .txt
-    //       Escriu el text al .txt de l'arxiu implícit, i tanca el .txt.
-    
-    // PRE: El .txt en concret ja està creat i l'arxiu té el nom
-    public void llegir_text() throws IOException {
-        BufferedReader br = obrir_fitxer_r();
-        String l;
-        if (br != null) {
-            while ((l = br.readLine()) != null) {
-                this.text = (this.text).concat(l); // anem enganxant al text cada linia 1 a 1 
-            }
-            br.close();
-        }
-    }
-    // POST:Llegeix el contingut del .txt i li assigna al text de l'arxiu
-    //      tanca el .txt;
-    
-    
-    
-    //  AQUÍ S'HA DE CANVIAR, LA LECTURA D'ATRIBUTS S'HA DE FER PARTINT DEL
-    //  CONJUNT DE DOCUMENTS
-    public void llegir_atributs(int ident) throws IOException {
-        String id = Integer.toString(ident);
-        Registre reg = new Registre();
-        reg.llegir_registre();
-        String a = reg.get_atributs(id);    // a String a tenim la tira amb els atributs
-    }
 }
-
-    
-    /* 
-    esborrar,
-    buidar_text
-    guardar atributs - va al main
-    */
